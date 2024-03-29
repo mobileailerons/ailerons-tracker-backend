d437aa61fa062c35952c6a75951d2858b6eb2985
LouisCoutel <louiscoutel75@gmail.com>
2024-03-29 15:35:15 +0100

    WIP: Templating

--- ailerons_tracker_backend/__init__.py
+++ ailerons_tracker_backend/__init__.py
@@ -7,15 +7,10 @@ import jinja_partials
 from flask import Flask, request
 import postgrest
 from flask_cors import CORS
-from ailerons_tracker_backend.models.article_model import Article
 from ailerons_tracker_backend.models.individual_model import Individual, Context
-from ailerons_tracker_backend.csv_parser import csv_parser
-from ailerons_tracker_backend.geojson_generator.generator import Generator
-from ailerons_tracker_backend.blueprints.ind_select import ind_select
-from ailerons_tracker_backend.blueprints.home import home
+from ailerons_tracker_backend.blueprints.portal import portal
 from .upload_image import upload_image
-from .errors import CloudinaryError, GeneratorError, InvalidFile
-from .clients.supabase_client import supabase
+from .errors import InvalidFile
 
 
 def create_app(test_config=None):
@@ -48,103 +43,7 @@ def create_app(test_config=None):
     jinja_partials.register_extensions(app)
 
     # Register a blueprint => blueprint routes are now active
-    app.register_blueprint(home, url_prefix="/portal")
-    app.register_blueprint(ind_select, url_prefix="/htmx")
-
-    @app.post('/upload')
-    def upload_file():
-        """ Parse a CSV file and insert data in DB """
-
-        try:
-            # On récupère le nom/id de l'individu auquel correspondent les fichiers
-            associated_individual = request.form["ind-select"]
-            app.logger.warning(associated_individual)
-
-            # A priori on aurait deux fichiers donc j'ai donné un nouveau nom à celui ci "
-            file_name, file_path = csv_parser.prepare_csv(request)
-            csv_id = supabase.create_csv_log(file_name)
-
-            # plus besoin de la table jointe si j'ai bien compris mais je n'y ai pas touché
-            record_list, new_individual_id_list = csv_parser.parse_csv(
-                file_path, csv_id)
-
-            supabase.batch_insert("record", record_list)
-            supabase.batch_insert("individual_record_id",
-                                  new_individual_id_list)
-
-            os.remove(file_path)
-
-            generator = Generator()
-            generator.generate()
-
-            return "CSVs uploaded and geoJSONs generated", 200
-
-        # Erreur supabase
-        except postgrest.exceptions.APIError as e:
-            app.logger.error(e.message)
-            return e.message, 304
-
-        except GeneratorError as e:
-            app.logger.error(e.message)
-            return e.message, 500
-
-        except InvalidFile as e:
-            app.logger.error(e)
-            return e, 400
-
-    @app.post('/news')
-    def upload_article():
-        """ Parse form data and insert news article in DB """
-
-        try:
-            image_url = upload_image(request.files['newsImage'])
-            article_data = Article(request.form, image_url).upload()
-
-            return article_data, 200
-
-        except (InvalidFile, CloudinaryError) as e:
-            app.logger.error(e.message)
-            return e.message, 400
-
-        except postgrest.exceptions.APIError as e:
-            app.logger.error(e.message)
-            return e.message, 304
-
-    @app.post('/individual')
-    def create_individual():
-        try:
-            items = request.files.items(multi=True)
-            image_urls = []
-
-            for item in items:
-                image_url = upload_image(item[1])
-
-                image_urls.append(image_url)
-
-            ind_data = Individual(
-                request.form["indName"],
-                request.form["indSex"],
-                image_urls).upload()
-            ind_id = ind_data.get('id')
-
-            context_data = Context(ind_id, request.form).upload()
-
-            content = {
-                'message': 'Successfully uploaded new individual',
-                'Individual data': ind_data,
-                'Context': context_data
-            }
-
-            return content, 200
-
-        except postgrest.exceptions.APIError as e:
-            app.logger.error(e.message)
-            return e.message, 304
-
-        except InvalidFile as e:
-            app.logger.error(e.message)
-            return e.message, 400
-
+    app.register_blueprint(portal)
     # Get a very useful log of all routes urls when running the server
     app.logger.warning(app.url_map)
 
--- tests/ind_select_tests.py
+++ tests/ind_select_tests.py
@@ -4,14 +4,14 @@ from pathlib import Path
 from flask import render_template
 resources = Path(__file__).parent / "resources"
 
-test_file = (resources / "ind_select_test.html").open("r")
+test_file = (resources / "ind_select_test.jinja").open("r")
 
 
 def test_ind_select(app):
     """ Test if ind names are rendered. """
 
     with app.app_context():
-        select = render_template('ind_select.html', inds=[
+        select = render_template('ind_select.jinja', inds=[
                                  {"name": "test"},
                                  {"name": "test2"}])
 
