f0d88b04168fa04f780e38949515c1881411c27a
onatyr <onat.rigault@gmail.com>
2024-04-01 23:42:50 +0200

    deleted date_to_iso in record model

--- ailerons_tracker_backend/models/record_model.py
+++ ailerons_tracker_backend/models/record_model.py
@@ -1,17 +1,10 @@
 """ Record Model """
 
-from datetime import datetime
 from ailerons_tracker_backend.models.record_field_model import LocalisationField, DepthField
 
 
 class Record:
     """ Model for a GPS data record. """
-
-    @staticmethod
-    def date_to_iso(excel_date_string):
-        """ Parses an excel D/M/Y H:M date string and returns an ISO formatted datestring. """
-
-        return datetime.strptime(excel_date_string, "%d/%m/%Y %H:%M").isoformat()
 
     def __init__(self, localisation_field_record: LocalisationField, depth_field_record: DepthField):
         self.latitude = localisation_field_record.latitude
