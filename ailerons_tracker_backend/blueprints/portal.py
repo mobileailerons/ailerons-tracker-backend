d725a461f8c70b99432664c5cc77bc256e9ae51c
LouisCoutel <louiscoutel75@gmail.com>
2024-04-18 23:29:38 +0200

    feat: add 'login' blueprint as a child in 'portal' blueprint

--- ailerons_tracker_backend/blueprints/portal.py
+++ ailerons_tracker_backend/blueprints/portal.py
@@ -4,12 +4,14 @@ from flask import Blueprint, abort, current_app, render_template
 from jinja2 import TemplateNotFound
 from ailerons_tracker_backend.blueprints.dashboard import dashboard
 from ailerons_tracker_backend.blueprints.csv_upload import csv_upload
+from ailerons_tracker_backend.blueprints.login import login
 
 portal = Blueprint('portal', __name__,
                    template_folder='templates', static_folder='static', url_prefix='/portal')
 
 portal.register_blueprint(dashboard)
 portal.register_blueprint(csv_upload)
+portal.register_blueprint(login)
 
 
 @portal.route('/')
