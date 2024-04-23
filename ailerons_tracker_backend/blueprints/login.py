69581f830c6e51f84ef7179a877b0af772542de3
LouisCoutel <louiscoutel75@gmail.com>
2024-04-23 21:29:29 +0200

    refac: formatting, update requirements.txt, raise exceptions

--- ailerons_tracker_backend/blueprints/login.py
+++ ailerons_tracker_backend/blueprints/login.py
@@ -1,6 +1,7 @@
 """ Login blueprint """
 
 from ailerons_tracker_backend.models.user_model import User
+from ailerons_tracker_backend.errors import EnvVarError
 from jinja_partials import render_partial
 from jinja2 import TemplateNotFound
 from flask import current_app, Blueprint, abort, redirect, render_template, request, url_for
@@ -14,17 +15,26 @@ load_dotenv()
 login = Blueprint('login', __name__,
                   template_folder='templates', url_prefix='login')
 
-
 @login.post('/')
 def connect():
     """ Connect to the app """
-    provided_pwd = request.form["password"]
 
-    if User.check_pwd(provided_pwd):
-        flask_login.login_user(User())
-        return redirect(url_for('portal.dashboard.show')), 200
+    try:
+        provided_pwd = request.form["password"]
 
-    return render_partial('login/login_section.jinja', error_message=True)
+        if User.check_pwd(provided_pwd):
+            flask_login.login_user(User())
+            return redirect(url_for('portal.dashboard.show')), 200
+
+        return render_partial('login/login_section.jinja', error_message=True)
+
+    except TemplateNotFound as e:
+        current_app.logger.warning(e)
+        abort(404)
+
+    except EnvVarError as e:
+        current_app.logger.warning(e)
+        return e, 500
 
 
 @login.get('/')
@@ -32,6 +42,7 @@ def show():
     """ Retrieve the login section HTML template """
 
     htmx = HTMX(current_app)
+
     try:
         if htmx:
             return make_response(
--- tests/login_tests.py
+++ tests/login_tests.py
@@ -1,3 +1,5 @@
+""" Test suite for login route and user management """
+
 import os
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash
@@ -8,19 +10,24 @@ load_dotenv()
 
 
 def test_user():
+    """ Test if user instatiates properly """
+
     usr = User()
-    assert User.id == 'Admin'
+    assert usr.id == 'Admin'
 
 
 def test_get_login_page(client):
+    """ Test getting the login page template """
 
     headers = {'HTTP_HX-Request': 'true'}
     response = client.get('/portal/login/', headers=headers)
-    logging.error(response)
+
     assert response.status_code == 200
 
 
 def test_successful_login(client):
+    """ Test login with a valid password """
+
     pwd = os.getenv('ADMIN_PWD')
 
     if pwd:
@@ -38,6 +45,8 @@ def test_successful_login(client):
 
 
 def test_invalid_pwd(client):
+    """ Test unsuccessful login attempt """
+
     pwd = 'not a valid pwd'
     headers = {'HTTP_HX-Request': 'true'}
 
